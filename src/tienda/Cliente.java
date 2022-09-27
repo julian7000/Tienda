2b3583c50c28e329d1a2e07e2cc021a1ed9a3bb8
julian7000 <baiedjulianet36@gmail.com>
2022-09-27 08:21:58 -0300

    baied

---
+++ src/tienda/Cliente.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tienda;
+
+/**
+ *
+ * @author ET36
+ */
+public class Cliente {
+    private String nombre;
+    private String apellido;
+    private int DNI;
+
+    public Cliente(String nombre, String apellido, int DNI) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.DNI = DNI;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getDNI() {
+        return DNI;
+    }
+
+    public void setDNI(int DNI) {
+        this.DNI = DNI;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente:" + "" +
+                "\nnombre:" + nombre + ""
+                + "\napellido=" + apellido + ""
+                + "\nDNI=" + DNI;
+    }
+}
